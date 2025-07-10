4c16ba456a20555699ef0cf76f9c23e77ea00be3
ryubin0509 <bin00509@naver.com>
2025-07-10 20:27:08 +0900

    [유빈] Security 제외

--- shoppingmall/src/main/java/com/example/shoppingmall/security/Security.java
+++
@@ -1,5 +0,0 @@
-package com.example.shoppingmall.security;
-
-public class Security {
-
-}
---
+++ shoppingmall/src/main/java/com/example/shoppingmall/security/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.example.shoppingmall.security;
+
+/*import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;*/
+
+/*@Configuration
+@EnableWebSecurity*/
+public class SecurityConfig {
+	
+	
+	/*
+	 * SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws
+	 * Exception { httpSecurity.csrf((csrfConfigure) ->(csrfConfigure).disable());
+	 * 
+	 * 
+	 * httpSecurity.authorizeHttpRequests((a)->
+	 * a.requestMatchers("/","WEB-INF/view/**","/").permitAll()
+	 * 
+	 * );
+	 * 
+	 * 
+	 * 
+	 * 
+	 * 
+	 * httpSecurity.build(); }
+	 */
+}
